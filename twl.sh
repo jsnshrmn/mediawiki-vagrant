87b3f8eeda91a9fd7e50ddf97a6217452f702251
Jason Sherman <jsn.sherman@gmail.com>
2021-03-23 18:54:49 +0000

    twl.sh downloads extensions if necessary

--- twl.sh
+++ twl.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-working_dir=$(pwd)
+# download extensions
+cd /vagrant/mediawiki/extensions/
+if [ ! -d CentralAuth ]; then
+git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/CentralAuth
+fi
+if [ ! -d Echo ]; then
+git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/Echo
+fi
+if [ ! -d GlobalPreferences ]; then
+git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/GlobalPreferences
+fi
+if [ ! -d TheWikipediaLibrary ]; then
+git clone ssh://gerrit.wikimedia.org:29418/mediawiki/extensions/TheWikipediaLibrary.git
+fi
 cd /vagrant/mediawiki
+# run migrations for extensions
 sudo -i bash -c "mysql < /vagrant/twl.sql;"
 mwscript maintenance/sql.php --wikidb centralauth extensions/CentralAuth/central-auth.sql;
 mwscript maintenance/sql.php --wikidb centralauth extensions/GlobalPreferences/sql/mysql/tables-generated.sql;
 mwscript extensions/CentralAuth/maintenance/migratePass0.php
 mwscript extensions/CentralAuth/maintenance/migratePass1.php
 mwscript maintenance/update.php --quick;
-cd $working_dir
